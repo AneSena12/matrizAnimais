f7e27d4c9d3e3f59bdb16be9b9024d065059af1a
AneSena12 <140628944+AneSena12@users.noreply.github.com>
2023-08-05 19:28:25 -0300

    Add files via upload

---
+++ matriz.js
@@ -0,0 +1,10 @@
+//Nesta atividade, você deve criar um código em JavaScript com uma matriz bidimensional para armazenar informações sobre animais e as suas características.
+//Cada linha deve representar um animal e cada coluna terá as diferentes informações sobre ele, como nome, espécie e idade.
+
+let matrizAnimais = [
+    ["Lily", "Canis lupus familiaris", 11],
+    ["Selina", "Felis catus", 3],
+    ["Jinx", "Felis catus", 3]
+]
+
+console.log(matrizAnimais)
